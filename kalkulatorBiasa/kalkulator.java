6c22b42c3e8a2848aeee01247e985abcf6c831bc
BlodotSakti <160871256+BlodotSakti@users.noreply.github.com>
2025-05-11 12:11:53 +0700

    Update kalkulator.java

--- kalkulatorBiasa/kalkulator.java
+++ kalkulatorBiasa/kalkulator.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+// Kalkulator ini hanya bisa perkalian, pembagian, pengurangan dan penjumlahan
+
 public class kalkulator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
